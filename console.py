07f914d06a00719161ba58d884f41dfe84c721c8
FTCP_ <97343988+FTCP9@users.noreply.github.com>
2023-08-18 01:09:06 +0200

    Update console.py

--- console.py
+++ console.py
@@ -2,5 +2,6 @@ From colorama improt Fore
 
 def main():
   print(f"{Fore.RED} Hello World") // First hello world print with red color
+  print(f"{Fore.YELLOW} Hello World") // second hello world print with yellow color
 
 main()
